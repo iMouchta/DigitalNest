44e0cf4bd504567657f99120f6c8af763dff6e51
Anstayker <Christian.rojas.bl@gmail.com>
2024-05-06 23:16:51 -0400

    visualizacion de solicitudes

--- digital-nest/src/components/ViewSolicitudes.js
+++ digital-nest/src/components/ViewSolicitudes.js
@@ -18,10 +18,13 @@ export default function ViewSolicitudes() {
   };
   const [data, setData] = useState([]);
 
+  const [solicitudes, setSolicitudes] = useState([]);
+
   useEffect(() => {
     fetch('http://localhost:8000/solicitudes')
       .then(response => response.json())
-      .then(data => setData(data));
+      .then(data => setSolicitudes(data))
+      .catch(error => console.error('Error:', error));
   }, []);
 
   const tmpData = [
@@ -45,53 +48,69 @@ export default function ViewSolicitudes() {
   ];
 
   return (
-    <List
-      sx={{
-        width: "100%",
-        maxWidth: 450,
-        bgcolor: "background.paper",
-        position: "relative",
-        overflow: "auto",
-        maxHeight: 600,
-        "& ul": { padding: 0 },
-      }}
-    >
-      {tmpData.map((item) => (
-        <div key={item.id}>
-          <ListItemButton onClick={() => handleClick(item.id)}>
-            <Card sx={{ width: "100%" }}>
-              <CardContent>
-                <Typography variant="body1">
-                  Nombre del docente: {item.nombre}
-                </Typography>
-                <Typography variant="body1">Aula: {item.aula}</Typography>
-                <Typography variant="body1">Fecha: {item.fecha}</Typography>
-                {open[item.id] ? <ExpandLess /> : <ExpandMore />}
-              </CardContent>
-              <Collapse in={open[item.id]} timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography variant="body1">
-                    Capacidad: {item.capacidad}
-                  </Typography>
-                  <Typography variant="body1">Motivo: {item.motivo}</Typography>
-                  <Typography variant="body1">
-                    Hora inicial: {item.horaInicial}
-                  </Typography>
-                  <Typography variant="body1">
-                    Hora final: {item.horaFinal}
-                  </Typography>
-                  <Typography variant="body1">
-                    Fecha de solicitud: {item.fechaSolicitud}
-                  </Typography>
-                  <Typography variant="body1">
-                    Hora de solicitud: {item.horaSolicitud}
-                  </Typography>
-                </CardContent>
-              </Collapse>
-            </Card>
-          </ListItemButton>
-        </div>
-      ))}
-    </List>
+
+    <div>
+    {solicitudes.map((solicitud, index) => (
+      <div key={index}>
+        <p>Id Solicitud: {solicitud.idsolicitud}</p>
+        <p>Id Materia: {solicitud.idmateria}</p>
+        <p>Id Ambiente: {solicitud.idambiente}</p>
+        <p>Capacidad Solicitud: {solicitud.capacidadsolicitud}</p>
+        <p>Fecha Solicitud: {solicitud.fechasolicitud}</p>
+        <p>Hora Inicial Solicitud: {solicitud.horainicialsolicitud}</p>
+        <p>Hora Final Solicitud: {solicitud.horafinalsolicitud}</p>
+        {/* Añade más campos según sea necesario */}
+      </div>
+    ))}
+  </div>
+
+    // <List
+    //   sx={{
+    //     width: "100%",
+    //     maxWidth: 450,
+    //     bgcolor: "background.paper",
+    //     position: "relative",
+    //     overflow: "auto",
+    //     maxHeight: 600,
+    //     "& ul": { padding: 0 },
+    //   }}
+    // >
+    //   {tmpData.map((item) => (
+    //     <div key={item.id}>
+    //       <ListItemButton onClick={() => handleClick(item.id)}>
+    //         <Card sx={{ width: "100%" }}>
+    //           <CardContent>
+    //             <Typography variant="body1">
+    //               Nombre del docente: {item.nombre}
+    //             </Typography>
+    //             <Typography variant="body1">Aula: {item.aula}</Typography>
+    //             <Typography variant="body1">Fecha: {item.fecha}</Typography>
+    //             {open[item.id] ? <ExpandLess /> : <ExpandMore />}
+    //           </CardContent>
+    //           <Collapse in={open[item.id]} timeout="auto" unmountOnExit>
+    //             <CardContent>
+    //               <Typography variant="body1">
+    //                 Capacidad: {item.capacidad}
+    //               </Typography>
+    //               <Typography variant="body1">Motivo: {item.motivo}</Typography>
+    //               <Typography variant="body1">
+    //                 Hora inicial: {item.horaInicial}
+    //               </Typography>
+    //               <Typography variant="body1">
+    //                 Hora final: {item.horaFinal}
+    //               </Typography>
+    //               <Typography variant="body1">
+    //                 Fecha de solicitud: {item.fechaSolicitud}
+    //               </Typography>
+    //               <Typography variant="body1">
+    //                 Hora de solicitud: {item.horaSolicitud}
+    //               </Typography>
+    //             </CardContent>
+    //           </Collapse>
+    //         </Card>
+    //       </ListItemButton>
+    //     </div>
+    //   ))}
+    // </List>
   );
 }
