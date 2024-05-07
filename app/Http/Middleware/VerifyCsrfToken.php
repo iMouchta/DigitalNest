23651cbb3af59ccba1b880352083b67857ce4203
iMouchta <jonathanperaltaflores@gmail.com>
2024-05-06 23:21:28 -0400

    errorMiddleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
-        'solicitud',
+        '/', // Ruta para crear y almacenar una solicitud
+        '/solicitudes', // Ruta para obtener todas las solicitudes
+        '/solicitud/*', // Rutas generadas por el resource controller SolicitudController
     ];
 }
