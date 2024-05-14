5f523a7b015df2b23b0f8617c307b33651696dc0
Anstayker <Christian.rojas.bl@gmail.com>
2024-05-14 05:05:54 -0400

    atender solicitudes view

--- digital-nest/src/components/ViewSolicitudEspecial.js
+++ digital-nest/src/components/ViewSolicitudEspecial.js
@@ -1,46 +1,59 @@
-import React, {useEffect, useState} from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
 
 export default function ViewSolicitudEspecial() {
-
-
   const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8000/solicitudes')
-      .then(response => response.json())
-      .then(data => setRows(data))
-      .catch(error => console.error('Error:', error));
+    fetch("http://localhost:8000/solicitudes")
+      .then((response) => response.json())
+      .then((data) => setRows(data))
+      .catch((error) => console.error("Error:", error));
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID de Solicitud</TableCell>
-            <TableCell>Ambiente</TableCell>
-            <TableCell>Fecha</TableCell>
-            <TableCell>Hora Inicial</TableCell>
-            <TableCell>Fecha Final</TableCell>
-            <TableCell>Acciones</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.ambiente}</TableCell>
-              <TableCell>{row.fecha}</TableCell>
-              <TableCell>{row.horaInicial}</TableCell>
-              <TableCell>{row.fechaFinal}</TableCell>
-              <TableCell>
-                <Button variant="contained" color="primary">Aprobar</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+<TableContainer component={Paper}>
+  <Table sx={{minWidth: 650}}>
+    <TableHead>
+      <TableRow>
+        <TableCell>ID de Solicitud</TableCell>
+        <TableCell>Solicitud</TableCell>
+        <TableCell>Fecha</TableCell>
+        <TableCell>Hora Inicial</TableCell>
+        <TableCell>Hora final</TableCell>
+        <TableCell>Aceptar Solicitud</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {rows.map((row) => (
+        <TableRow key={row.idsolicitud}>
+          <TableCell>{row.idsolicitud}</TableCell>
+          <TableCell>{row.nombreAmbiente}</TableCell>
+          <TableCell>{row.fechasolicitud}</TableCell>
+          <TableCell>
+            {row.horainicialsolicitud.split(":").slice(0, 2).join(":")}
+          </TableCell>
+          <TableCell>
+            {row.horafinalsolicitud.split(":").slice(0, 2).join(":")}
+          </TableCell>
+          <TableCell>
+            <Button variant="contained" color="primary">
+              Aceptar
+            </Button>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
   );
 }
