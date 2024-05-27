8f66deaad8803d8f33956c1087c872edb874e6db
iMouchta <jonathanperaltaflores@gmail.com>
2024-05-27 14:12:57 -0400

    Cambio de nombre a Modelos

---
+++ app/Models/Motivo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Motivo extends Model
+{
+    use HasFactory;
+    protected $table = 'motivo';
+    protected $primaryKey = 'idmotivo';
+
+    public $timestamps = false;
+    protected $fillable = [
+        'idmateria',
+        'nombremotivo',
+        'registrado',
+    ];
+    public function materia()
+    {
+        return $this->belongsTo(Materia::class, 'idmateria', 'idmateria');
+    }
+
+}
