a4cdc755bcf7cc5d4bca925ed80042f0b0868e21
Anstayker <Christian.rojas.bl@gmail.com>
2024-05-28 07:11:08 -0400

    responder solicitud

--- digital-nest/src/components/ViewSolicitudEspecial.js
+++ digital-nest/src/components/ViewSolicitudEspecial.js
@@ -1,144 +1,341 @@
-import React, { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Button,
-} from "@mui/material";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
-import { toast, Toaster } from "react-hot-toast";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 
-export default function ViewSolicitudEspecial() {
-  const [open, setOpen] = useState(false);
-  const [idsolicitudToAccept, setIdsolicitudToAccept] = useState(null);
-  const [rows, setRows] = useState([]);
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Collapse from "@mui/material/Collapse";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-  const handleClickOpen = (idsolicitud) => {
-    setOpen(true);
-    setIdsolicitudToAccept(idsolicitud);
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import { set } from "lodash";
+
+const columns = [
+  {
+    id: "idsolicitud",
+    label: "ID",
+    minWidth: 170,
+  },
+  {
+    id: "motivosolicitud",
+    label: "Motivo Solicitud",
+    minWidth: 170,
+    align: "center",
+  },
+  { id: "fechaSolicitud", label: "Fecha de Reserva", minWidth: 100 },
+  {
+    id: "horainicialsolicitud",
+    label: "Hora Inicial",
+    minWidth: 100,
+    align: "right",
+  },
+  {
+    id: "horafinalsolicitud",
+    label: "Hora Final",
+    minWidth: 100,
+    align: "right",
+  },
+  {
+    id: "nombreadministrador",
+    label: "Administrador",
+    minWidth: 170,
+    align: "right",
+  },
+];
+
+function createData(
+  idsolicitud,
+  nombreadministrador,
+  fechaSolicitud,
+  horainicialsolicitud,
+  horafinalsolicitud,
+  motivosolicitud,
+  ambientes
+) {
+  return {
+    idsolicitud,
+    nombreadministrador,
+    fechaSolicitud,
+    horainicialsolicitud,
+    horafinalsolicitud,
+    motivosolicitud,
+    ambientes,
   };
+}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+export default function ViewSolicitudEspecial({ solicitudes }) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [expandedRow, setExpandedRow] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [solicitudesAfectadas, setSolicitudesAfectadas] = React.useState([]);
 
-  const fetchSolicitudes = () => {
-    fetch("http://localhost:8000/solicitudes")
-      .then((response) => response.json())
-      .then((data) => setRows(data))
-      .catch((error) => console.error("Error:", error));
-  };
+  const rows = solicitudes.map((solicitud) =>
+    createData(
+      solicitud.idsolicitud,
+      solicitud.nombreusuarios[0],
+      solicitud.fechasolicitud,
+      solicitud.horainicialsolicitud.split(":").slice(0, 2).join(":"),
+      solicitud.horafinalsolicitud.split(":").slice(0, 2).join(":"),
+      solicitud.motivosolicitud,
+      solicitud.ambientes
+    )
+  );
 
-  useEffect(() => {
-    fetchSolicitudes();
-  }, []);
-
-  const handleAccept = (idsolicitud) => {
-    fetch("http://localhost:8000/api/aceptar", {
+  const handleClick = (solicitudId) => {
+    const response = fetch("http://localhost:8000/api/confirmacion", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        idsolicitud: idsolicitud,
-        // Agrega aquí el resto de los datos que necesitas enviar
+        idsolicitud: solicitudId,
       }),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error en la solicitud POST");
+        }
+        return response.json();
+      })
       .then((data) => {
-        toast.success("Se aceptó la reserva con éxito", {
-          duration: 7000,
-        });
-        console.log("Success:", data);
-        fetchSolicitudes(); // Actualiza las solicitudes después de la petición POST
+        setSolicitudesAfectadas(data.SolicitudesEliminar);
+        setOpen(true);
       })
       .catch((error) => {
-        toast.error("No se pudo aceptar la reserva");
         console.error("Error:", error);
       });
   };
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }}>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID de Solicitud</TableCell>
-            <TableCell>Ambiente</TableCell>
-            <TableCell>Fecha de Reserva</TableCell>
-            <TableCell>Hora Inicial</TableCell>
-            <TableCell>Hora final</TableCell>
-            <TableCell>Aceptar Solicitud</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows
-            .filter((row) => !row.aceptada)
-            .sort(
-              (a, b) => new Date(a.fechasolicitud) - new Date(b.fechasolicitud)
-            )
-            .map((row) => (
-              <TableRow key={row.idsolicitud}>
-                <TableCell>{row.idsolicitud}</TableCell>
-                <TableCell>{row.nombreAmbiente}</TableCell>
-                <TableCell>{row.fechasolicitud}</TableCell>
-                <TableCell>
-                  {row.horainicialsolicitud.split(":").slice(0, 2).join(":")}
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              {/* Celda de tabla vacía para el icono de expansión */}
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  // style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
                 </TableCell>
-                <TableCell>
-                  {row.horafinalsolicitud.split(":").slice(0, 2).join(":")}
-                </TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => handleClickOpen(row.idsolicitud)}
-                  >
-                    Aceptar
-                  </Button>
-                  <Dialog
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                  >
-                    <DialogTitle id="alert-dialog-title">
-                      {"Confirmación"}
-                    </DialogTitle>
-                    <DialogContent>
-                      <DialogContentText id="alert-dialog-description">
-                        ¿Está seguro de que quieres aceptar esta solicitud?
-                      </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                      <Button onClick={handleClose}>Cancelar</Button>
-                      <Button
-                        onClick={() => {
-                          handleAccept(idsolicitudToAccept);
-                          handleClose();
-                        }}
-                        autoFocus
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows &&
+              rows
+                .sort(
+                  (a, b) =>
+                    new Date(a.fechasolicitud) - new Date(b.fechasolicitud)
+                )
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, indexRow) => {
+                  return (
+                    <React.Fragment key={indexRow}>
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={indexRow}
                       >
-                        Confirmar
-                      </Button>
-                    </DialogActions>
-                  </Dialog>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-      <Toaster />
-    </TableContainer>
+                        <TableCell>
+                          <IconButton
+                            aria-label="expand row"
+                            size="small"
+                            onClick={() =>
+                              setExpandedRow(
+                                expandedRow !== indexRow ? indexRow : null
+                              )
+                            }
+                          >
+                            {expandedRow === indexRow ? (
+                              <KeyboardArrowUpIcon />
+                            ) : (
+                              <KeyboardArrowDownIcon />
+                            )}
+                          </IconButton>
+                        </TableCell>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                      <TableRow>
+                        <TableCell
+                          style={{ paddingBottom: 0, paddingTop: 0 }}
+                          colSpan={6}
+                        >
+                          <Collapse
+                            in={expandedRow === indexRow}
+                            timeout="auto"
+                            unmountOnExit
+                          >
+                            <Box margin={1}>
+                              <Typography
+                                variant="h6"
+                                gutterBottom
+                                component="div"
+                              >
+                                Ambientes reservados
+                              </Typography>
+                              <Table size="small" aria-label="ambientes">
+                                <TableHead>
+                                  <TableRow>
+                                    <TableCell>Nombre del ambiente</TableCell>
+                                    <TableCell>Edificio</TableCell>
+                                    <TableCell>Planta</TableCell>
+                                  </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                  {expandedRow === indexRow &&
+                                    row.ambientes &&
+                                    row.ambientes.map(
+                                      (ambiente, indexAmbiente) => (
+                                        <TableRow key={indexAmbiente}>
+                                          <TableCell component="th" scope="row">
+                                            {ambiente.nombre_ambiente}
+                                          </TableCell>
+                                          <TableCell>
+                                            {ambiente.edificio}
+                                          </TableCell>
+                                          <TableCell>
+                                            {ambiente.planta}
+                                          </TableCell>
+                                        </TableRow>
+                                      )
+                                    )}
+                                </TableBody>
+                              </Table>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                style={{ marginTop: "20px" }}
+                                onClick={() => handleClick(row.idsolicitud)}
+                              >
+                                Aceptar
+                              </Button>
+                              <Dialog
+                                open={open}
+                                onClose={() => setOpen(false)}
+                              >
+                                <DialogTitle>
+                                  ¿Estás seguro que quieres aceptar esta
+                                  solicitud?
+                                </DialogTitle>
+                                <DialogContent>
+                                  {solicitudesAfectadas.length === 0 ? (
+                                    <p>
+                                      Está acción no afectará a ninguna otra
+                                      solicitud
+                                    </p>
+                                  ) : (
+                                    <div>
+                                      <p>
+                                        La acción afectará a las siguientes
+                                        solicitudes ya realizadas con los IDs:
+                                      </p>
+                                      <ul>
+                                        {solicitudesAfectadas.map(
+                                          (item, index) => (
+                                            <li key={index}>{item}</li>
+                                          )
+                                        )}
+                                      </ul>
+                                    </div>
+                                  )}
+                                </DialogContent>
+                                <DialogActions>
+                                  <Button
+                                    onClick={() => setOpen(false)}
+                                    color="primary"
+                                  >
+                                    Cancelar
+                                  </Button>
+                                  <Button
+                                    onClick={async () => {
+
+                                      await fetch('http://localhost:8000/api/accept', {
+                                        method: 'POST',
+                                        headers: {
+                                          'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify({ 
+                                          idsolicitud: row.idsolicitud,
+                                        })
+                                      })
+                                      .then(response => {
+                                        if (!response.ok) {
+                                          throw new Error('Error en la solicitud POST');
+                                        }
+                                        return response.json();
+                                      })
+                                      .then(data => {
+                                        console.log(data);
+                                        setOpen(false);
+                                      })
+                                      .catch(error => {
+                                        console.error('Error:', error);
+                                      });
+                                      setOpen(false);
+                                    }}
+                                    color="primary"
+                                  >
+                                    Confirmar
+                                  </Button>
+                                </DialogActions>
+                              </Dialog>
+                            </Box>
+                          </Collapse>
+                        </TableCell>
+                      </TableRow>
+                    </React.Fragment>
+                  );
+                })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        labelRowsPerPage="Filas por página:"
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
--- digital-nest/src/pages/ResponderSolicitudPage.js
+++ digital-nest/src/pages/ResponderSolicitudPage.js
@@ -1,12 +1,20 @@
-import { Button } from "@mui/material";
-import React from "react";
-import ViewSolicitudEspecial from "../components/ViewSolicitudEspecial.js";
+import React, { useEffect, useState } from "react";
+import ViewSolicitudEspecial from "../components/ViewSolicitudEspecial";
 
 export default function ResponderSolicitudPage() {
+  const [solicitudesEspeciales, setSolicitudesEspeciales] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/solicitudEspecial")
+      .then((response) => response.json())
+      .then((data) => setSolicitudesEspeciales(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
     <div>
-      <h1>Atender solicitudes</h1>
-      <ViewSolicitudEspecial />
+      <h2>Responder solicitudes especiales</h2>
+      <ViewSolicitudEspecial solicitudes={solicitudesEspeciales} />
     </div>
   );
 }
